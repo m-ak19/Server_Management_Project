0848260b729e158172b3dd369abac92bd3955385
Allan Dubernard <allan.dubernard@gmail.com>
2024-05-30 20:16:40 +0200

    update

--- stat.h
+++ stat.h
@@ -1,10 +1,12 @@
 #ifndef STATH
 #define STATH
 
-int getNombreLigne(char* nom_fichier);
-int getNombreErreur(char* nom_fichier);
-int getNombreFichierRecus(char* nom_fichier);
-int ecrire_erreur(char* erreur, char* nom_fichier);
-int ecrire_fin_fichier_stat(char* texte_a_ecrire, char* nom_fichier);
+int getNombreErreur();
+int getNombreFichierRecus();
+void AffichNombreFichierRecus();
+void AffichNombreErreur();
+int ecrire_erreur(char* erreur);
+int ecrire_fichier_recu(char* nom_fichier_recu);
+
 
 #endif
